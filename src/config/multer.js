7b46ac137fa0bcd4e19b3cc914f481a22f18761f
JonasFilhoDev <jonasfilho1985@gmail.com>
2026-03-13 20:36:01 -0300

    correçao multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,6 +1,6 @@
 import multer from 'multer';
 import { v4 } from 'uuid';
-import { dirname } from 'node:path';
+import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 export default {
   storage: multer.diskStorage({
-    destination: '/tmp',
+    destination: resolve(__dirname, '..', '..', 'uploads'),
     filename: (_request, file, callback) => {
       const uniqueName = v4().concat(`-${file.originalname}`);
       return callback(null, uniqueName);
