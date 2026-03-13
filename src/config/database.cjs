79c521035f7e523a63dc4d54cedefb31bd8ae8db
JonasFilhoDev <jonasfilho1985@gmail.com>
2026-03-13 14:47:23 -0300

    correção api para conexao com banco

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,10 +1,16 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: 'dpg-d6q4hnp5pdvs738pdne0-a.oregon-postgres.render.com',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'dev-burguer-db',
+  username: 'devburguer_tjvv_user',
+  password: 'UxepZP4l4uzs4d4rv1GwRX0gA3lSshd1',
+  database: 'devburguer_tjvv',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   define: {
     timestamps: true,
     underscored: true,
