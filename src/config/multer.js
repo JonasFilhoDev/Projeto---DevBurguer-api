b7b1ab6a1284032f2b50da68fc2d9f63a4243d4d
JonasFilhoDev <jonasfilho1985@gmail.com>
2026-03-13 23:06:24 -0300

    correçao rotas

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import 'dotenv/config';
 
 
 cloudinary.config({
